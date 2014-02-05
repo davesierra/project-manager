50b904fe3d62db4f6d672f21af9a32cf9a903143
Dave Sierra <dave@hakusports.com>
2014-02-04 21:16:55 -0500

    Initial Commit
    After using rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = 'aa115be957ec85fb8aba534b68b332a95688055f38a841f55e2321f32c882bd97052b5d04b79316c1929cb6a19812d0035c00ea002ef7a05692763a983ffd892'

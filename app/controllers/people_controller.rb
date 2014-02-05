e3b54e4b74efa627c8cac74e099f7e9781617066
Dave Sierra <dave@hakusports.com>
2014-02-04 21:57:10 -0500

    Controller editing

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,24 @@
 class PeopleController < ApplicationController
 
+	before_filter :authenticate, :only => :show
 
+	def index
+		@people = [
+			'luke-skywalker',
+			'darth-vader'
+		]
+	end
+
+	def show
+		@person = params[:id]
+	end
+
+	private
+
+	def authenticate
+		if params[:id] == 'darth-vader'
+			redirect_to people_path(), :notice => 'Page is restricted'
+		end
+	end
 
 end
